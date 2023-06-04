a92c887a65156dd280966bd07a7ca1069abdc353
Adedayo Adewale <atwale44@yahoo.com>
2023-06-04 17:26:22 +0100

    update html and css

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Sound Engineer',
+    'location': 'Lagos, Nigeria',
+    'salary': '#500,000.00'
+  },
+  {
+    'id': 2,
+    'title': 'Talent Manager',
+    'location': 'Lagos, Nigeria',
+    'salary': '#500,000.00'
+
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Remote',
+    'salary': '$50,000.00'
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template("index.html")
+  return render_template("index.html",jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
