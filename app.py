a700baee3810be574df856f652d3b7728bae4670
Adedayo Adewale <atwale44@yahoo.com>
2023-06-07 07:06:11 +0100

    make dynamic changes for each job application

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def cs_careers():
   jobs = load_jobs_from_db()
-  return render_template("index.html",jobs=jobs,
-  company_name='Cruel Station' )
+  return render_template("index.html",jobs=jobs)
 
 @app.route("/api/jobs")
 
@@ -15,6 +14,13 @@ def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+   job = load_job_from_db(id)
 
+   if not job:
+      return "Oops! Can't find what you are looking for!", 404
+   
+   return render_template('jobpage.html', job=job)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
